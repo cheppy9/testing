b7492ac6ef20a749bcf362f7e536f51474bd461c
Taufiq Ridwan <taufiq26.ridwan@gmail.com>
2022-07-19 18:54:53 +0700

    login & register view

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,6 +4,18 @@ function Login() {
   return (
     <main style={{ padding: "1rem 0" }} className="App-header">
       <h2>Login Page</h2>
+
+      <form className="box-form">
+        <label fot="username">Username :</label>
+        <input type="text" id="username" className="form-control" />
+        <br/>
+
+        <label fot="username">Password :</label>
+        <input type="password" id="password" className="form-control" />
+        <br/><br/>
+
+        <button type="submit" className='btn btn-primary form-control'>MASUK!</button>
+      </form>
     </main>
   );
 }
--- src/routes/register.js
+++ src/routes/register.js
@@ -4,6 +4,30 @@ function Register() {
   return (
     <main style={{ padding: "1rem 0" }} className="App-header">
       <h2>Register Page</h2>
+
+      <form className="box-form">
+        <label fot="nama_lengkap">Nama Lengkap :</label>
+        <input type="text" id="nama_lengkap" className="form-control" />
+        <br/>
+        
+        <label fot="alamat">Alamat :</label>
+        <input type="text" id="alamat" className="form-control" />
+        <br/>
+        
+        <label fot="telepon">Telepon :</label>
+        <input type="text" id="telepon" className="form-control" />
+        <br/>
+        
+        <label fot="username">Username :</label>
+        <input type="text" id="username" className="form-control" />
+        <br/>
+
+        <label fot="username">Password :</label>
+        <input type="password" id="password" className="form-control" />
+        <br/><br/>
+
+        <button type="submit" className='btn btn-primary form-control'>DAFTAR!</button>
+      </form>
     </main>
   );
 }
